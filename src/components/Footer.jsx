93a800f07f03810b8f09adcd2a0c3551a6851767
Faisal <faisalsagheerqureshi@gmail.com>
2025-01-13 14:40:37 +0500

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,40 +8,43 @@ export default function Footer() {
     return (
         <>
             <div className='bg-[#000b1c] text-white list-none'>
-                <div className="flex flex-wrap items-center lg:justify-between justify-center border-b-[.20px] border-b-white py-5 text-lg sm:text-lg lg:text-lg">
-                    <div className='flex flex-wrap lg:justify-between justify-center'>
-                        {
-                            footer1.map((item, index) => (
-                                <li className='px-5 pb-2 lg:last:pt-0' key={index}>
-                                    <a href={item.href} target={item.target}><i className={item.icon}></i>{item.No}</a>
-                                </li>
-                            ))
-                        }
-                    </div>
-
-                    <div className='flex pt-4 lg:pt-0'>
-
+                <div className="flex flex-wrap justify-center items-center lg:justify-between border-b-[.20px] border-b-white py-5 px-4 lg:px-8 text-lg">
+                    
+                    <div className='flex flex-wrap flex-col'>
+                        <div className='flex flex-col -mt-20 lg:-mt-0'>
+                            <Link smooth={true}
+                                offset={-120} duration={8000} to="Hero" className='flex flex-col items-center lg:items-start'>
+                                <img src="https://www.softechgoal.com/Logo.png" className='w-72 lg:-ml-16'/>
+                                <p className='text-xl sm:text-sm lg:text-lg -mt-20 lg:-mt-16 py-5 lg:py-0 px-3'>Empowering innovation and delivering cutting-edge solutions,<br/> 
+                                    our software startup is here to redefine the future of technology.</p>
+                            </Link>
+                        </div>
+                        <div className='flex pt-5 lg:pt-10 pb-5 lg:pb-0 justify-center lg:justify-start  pl-0 lg:pl-4'>
                         {
                             footer2.map((item, index) => (
-                                <li className='px-5' key={index}>
-                                    <a href={item.href} target={item.target}><i className={item.icon}></i>{item.No}</a>
+                                <li key={index}>
+                                    <a href={item.href} target={item.target} className='text-2xl'><i className={item.icon}></i></a>
+                                </li>
+                            ))
+                        }
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col pt-4 lg:pt-20'>                      
+                         {
+                            footer1.map((item, index) => (
+                                <li className='px-5 pb-2' key={index}>
+                                    <a href={item.href} target={item.target} className='text-2xl'><i className={item.icon}></i>{item.No}</a>
                                 </li>
                             ))
                         }
                     </div>
                 </div>
-                <div className='flex flex-wrap items-center lg:justify-between justify-center'>
-                    <div>
-                        <Link smooth={true}
-                            offset={-120} duration={8000} to="Hero" className='flex items-center'>
-                            <img src="https://www.softechgoal.com/Logo.png" className='h-32'/>
-                            <p className='text-sm sm:text-sm lg:text-lg'>© 2024 Softechgoal. All rights reserved.</p>
-                        </Link>
-                    </div>
-                    <div className='px-5'>
-                        <h5 className='underline'>Privacy Policy</h5>
-                    </div>
+
+                <div className='text-center'>
+                    <p className='p-10'>© 2024 Softechgoal. All rights reserved</p>
                 </div>
+
             </div>
         </>
     )
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -82,12 +82,12 @@ export const videos = [
 
 export const footer1 =[
   {icon:"fa-solid fa-phone px-2",No:"+1 (512) 889-7071"},
-  {icon:"fa-brands fa-whatsapp px-2",No:"+1 (512) 889-7071"},
+  {icon:"fa-brands fa-whatsapp px-2 text-[#52da7f]",No:"+1 (512) 889-7071"},
 ]
 
 export const footer2 = [
   {icon:"fa-brands fa-linkedin-in",href:"https://www.linkedin.com/company/softechgoal",target:"_blank"},
-  {icon:"fa-brands fa-instagram",href:"https://www.instagram.com/softechgoal?igsh=MWZqZ3Rxb2MxN2g5ZA==",target:"_blank"},
+  {icon:"fa-brands fa-instagram px-5",href:"https://www.instagram.com/softechgoal?igsh=MWZqZ3Rxb2MxN2g5ZA==",target:"_blank"},
   {icon:"fa-brands fa-facebook-f",href:"https://www.facebook.com/profile.php?id=61565552590000",target:"_blank"},
 
 ]
