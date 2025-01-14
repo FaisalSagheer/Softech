1cf886dfb053d5143b98d9a56fadb3f961c3e2ef
Faisal <faisalsagheerqureshi@gmail.com>
2025-01-15 00:47:36 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,52 +10,31 @@ import Portfolio from './components/Porfolio/Portfolio';
 import Banner from './components/Banner';
 import Email from './components/Contact Us/Email';
 import Footer from './components/Footer';
-import NotFound from './components/NotFound';
-import { Element } from 'react-scroll';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react'
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-  document.addEventListener("contextmenu",(e)=>{
+  document.addEventListener("contextmenu", (e) => {
     e.preventDefault();
   })
   return (
     <>
       <>
-      <ToastContainer theme='dark'/>
+        <ToastContainer theme='dark' />
 
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Element name="Hero">
-                    <Hero />
-                  </Element>
-                  <Element name="AboutUs">
-                    <AboutUs />
-                  </Element>
-                  <Element name="Services">
-                    <Services />
-                  </Element>
-                  <Element name="Portfolio">
-                    <Portfolio />
-                  </Element>
-                  <Banner />
-                  <Element name="Email">
-                    <Email />
-                  </Element>
-                  <Footer />
-                </>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Navbar />
+        <Hero />
+        <AboutUs />
+        <Services />
+        <Portfolio />
+        <Banner />
+        <Email />
+
+        <Footer />
       </>
+
+
     </>
 
   )
